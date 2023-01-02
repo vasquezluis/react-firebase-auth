a52b550bf71ee44d514266a619cb44404e9146cd
LuiiVasquez <luivasque95@gmail.com>
2023-01-02 15:52:59 -0600

    adding home, login & register Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline"> hello world </h1>;
+  return (
+    <div className="bg-slate-300 h-screen text-white flex">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
