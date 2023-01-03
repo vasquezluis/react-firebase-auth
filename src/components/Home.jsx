d9c650f02b64484a278c8dc71e2351b5c6c04e19
LuiiVasquez <luivasque95@gmail.com>
2023-01-03 14:21:44 -0600

    creating home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,28 @@
 import { useAuth } from "../context/authContext";
 
 function Home() {
-  return <div>Home</div>;
+  // instanciar valores de useAuth
+  const { user, logout, loading } = useAuth();
+
+  console.log(user);
+
+  const handleLogOut = async () => {
+    try {
+      await logout();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  if (loading) return <h1>Loading</h1>;
+
+  return (
+    <div>
+      <h1>Welcome: {user.email}</h1>
+
+      <button onClick={handleLogOut}>Logout</button>
+    </div>
+  );
 }
 
 export default Home;
