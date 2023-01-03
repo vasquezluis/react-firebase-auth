01cb2681c2381136224b65df18e21bc8b4ac6ca3
LuiiVasquez <luivasque95@gmail.com>
2023-01-03 10:26:53 -0600

    creating singup function & adding firebase auth connection

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,8 +1,12 @@
 import { createContext, useContext } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+// traer auth del archivo firebase
+import { auth } from "../firebase";
 
 // este si contiene el valor (user)
 export const authContext = createContext();
 
+// usar Auth
 export const useAuth = () => {
   const context = useContext(authContext);
   return context;
@@ -11,12 +15,13 @@ export const useAuth = () => {
 // permite acceder al valor desde cualquier lugar
 // proveedor y crear objetos
 export function AuthProvider({ children }) {
-  const user = {
-    login: true,
-    name: "luisvasquez",
-  };
+  
+  const singup = (email, password) =>
+    // ejectuar funcion de firebase (pasar un auth, email, password)
+    createUserWithEmailAndPassword(auth, email, password);
 
+  // exportar singup
   return (
-    <authContext.Provider value={{ user }}>{children}</authContext.Provider>
+    <authContext.Provider value={{ singup }}>{children}</authContext.Provider>
   );
 }
