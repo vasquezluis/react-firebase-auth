fc5cdd6d90d0833c940a31d7b630e22fa2ed4165
LuiiVasquez <luivasque95@gmail.com>
2023-01-03 10:25:46 -0600

    deleting useAuth() function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,7 @@
 import { useAuth } from "../context/authContext";
 
 function Home() {
-  const { user } = useAuth();
-
-  return <div>{user.name}</div>;
+  return <div>Home</div>;
 }
 
 export default Home;
