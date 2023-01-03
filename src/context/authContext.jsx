a6775fd1aa99c51c04356c627876cdc744e1a405
LuiiVasquez <luivasque95@gmail.com>
2023-01-03 14:24:07 -0600

    creating login, logout, auth user

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,5 +1,10 @@
-import { createContext, useContext } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
 // traer auth del archivo firebase
 import { auth } from "../firebase";
 
@@ -9,19 +14,52 @@ export const authContext = createContext();
 // usar Auth
 export const useAuth = () => {
   const context = useContext(authContext);
+  if (!context) throw new Error("There is no Auth provider");
   return context;
 };
 
 // permite acceder al valor desde cualquier lugar
 // proveedor y crear objetos
 export function AuthProvider({ children }) {
-  
+  // estado para contener las credenciales del usuario
+  const [user, setuser] = useState(null);
+
+  // loading para saber si esta cargando aun
+  const [loading, setLoading] = useState(true);
+
   const singup = (email, password) =>
     // ejectuar funcion de firebase (pasar un auth, email, password)
     createUserWithEmailAndPassword(auth, email, password);
 
-  // exportar singup
+  // consulta a firebase
+  const login = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // funcion para logout
+  const logout = () => signOut(auth);
+
+  // funcion para obtener las credenciales del usuario cada vez que cambia
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+
+      console.log(currentUser)
+
+      // almacenar las credenciales del usuario actual
+      setuser(currentUser);
+
+      // despues de obtener las credenciales
+      setLoading(false);
+    });
+
+    // ejecutar return cuando el componente es desmontado
+    return () => unsubscribe();
+  }, []);
+
+  // exportar singup y login, user
   return (
-    <authContext.Provider value={{ singup }}>{children}</authContext.Provider>
+    <authContext.Provider value={{ singup, login, user, logout, loading }}>
+      {children}
+    </authContext.Provider>
   );
 }
