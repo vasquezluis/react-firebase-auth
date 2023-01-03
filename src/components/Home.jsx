edcc65c16166ec7112624e88f9e49b43fc8a75db
LuiiVasquez <luivasque95@gmail.com>
2023-01-02 19:26:02 -0600

    using Auth context

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { useAuth } from "../context/authContext";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+  const { user } = useAuth();
+
+  return <div>{user.name}</div>;
 }
 
-export default Home
+export default Home;
